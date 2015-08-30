85a0c5ef62b354b57d118efca8986bca680a4654
Carryl Sorene <carrylsorene@gmail.com>
2015-08-30 20:15:04 +0100

    A few more passed

--- questions.rb
+++ questions.rb
@@ -6,8 +6,9 @@ end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
-  array.select { |string| str.start_with('a', 'e', 'i', 'o', 'u')}
+  array.select {|str| str.start_with /[aeiou]/}
 end
+#passed
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(array)
@@ -17,33 +18,41 @@ end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
-  array.compact.delete(false)
+  array.compact.delete_if {|str| str == false}
 end
+#passed
 
 # don't reverse the array, but reverse every word inside it. e.g.
 # ['dog', 'monkey'] becomes ['god', 'yeknom']
 def reverse_every_element_in_array(array)
-  array.reverse
+  array.reverse 
 end
+
+# plus { |e|  prints e, " " } ?
 
 # given an array of student names, like ['Bob', 'Dave', 'Clive']
 # give every possible pairing - in this case:
 # [['Bob', 'Clive'], ['Bob', 'Dave'], ['Clive', 'Dave']]
 # make sure you don't have the same pairing twice, 
-def every_possible_pairing_of_students(array)
-  
-end
+
+# def every_possible_pairing_of_students(array)
+#   array.map {|pair| pair.sort}.sort_by {|pair| [pair.first], [pair.last] [pairs.first..last]}
+# end
+
+#wth?
 
 # discard the first 3 elements of an array, 
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
-  
+  array.slice[2] 
 end
+# or {|e| e > 4}
 
 # add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
-  
+  array.insert(0,1)
 end
+# passed
 
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
@@ -61,10 +70,7 @@ end
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
-  return 0 - number
-end
-  puts number(5)
-  puts number(-7)
+  
 end
 
 
@@ -271,4 +277,3 @@ end
 def bottles_one
 
 end
-
