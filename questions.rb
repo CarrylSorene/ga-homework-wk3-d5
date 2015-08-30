8136d294c97e3d09ab82de3a3440cb3bc21dc4a1
Carryl Sorene <carrylsorene@gmail.com>
2015-08-30 21:53:41 +0100

    Passed a few more

--- questions.rb
+++ questions.rb
@@ -25,7 +25,7 @@ end
 # don't reverse the array, but reverse every word inside it. e.g.
 # ['dog', 'monkey'] becomes ['god', 'yeknom']
 def reverse_every_element_in_array(array)
-  array.reverse 
+  array.reverse
 end
 
 # plus { |e|  prints e, " " } ?
@@ -38,8 +38,6 @@ end
 # def every_possible_pairing_of_students(array)
 #   array.map {|pair| pair.sort}.sort_by {|pair| [pair.first], [pair.last] [pairs.first..last]}
 # end
-
-#wth?
 
 # discard the first 3 elements of an array, 
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
@@ -57,29 +55,31 @@ end
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
-  
+  array.sort_by {|str| str.end_with /A-Z/}
 end
 
 # cut strings in half, and return the first half, e.g.
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  string.slice(0..2)
   
 end
+# passed
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
-  
+  number.
 end
-
 
 # turn an array of numbers into two arrays of numbers, one an array of 
 # even numbers, the other an array of odd numbers
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
-  
+  array.each_slice {|str| str.even.to_a}
+  array.each_slice {|str| str.odd.to_a}
 end
 
 # count the numbers of elements in an element which are palindromes
@@ -87,17 +87,17 @@ end
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
 def number_of_elements_that_are_palindromes(array)
-  
+  array.count {|e| e.reverse}
 end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
-  
+  array.min_by { |str| str.length }
 end
 
 # return the longest word in an array
 def longest_word_in_array(array)
-  
+  array.max_by { |str| str.length }
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
@@ -114,7 +114,7 @@ end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
-  
+  symbol.to_str
 end
 
 # get the average from an array, rounded to the nearest integer
@@ -220,7 +220,7 @@ end
 
 # get the square root of a number
 def square_root_of(number)
-  
+  number.
 end
 
 # count the number of words in a file
@@ -276,4 +276,6 @@ end
 # (there's no RSpec test for this one)
 def bottles_one
 
+end
+end
 end
